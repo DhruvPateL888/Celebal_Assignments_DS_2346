e046617b917c5183f9c1d194e0ac47e2c44d6444
DhruvPateL888 <136485353+DhruvPateL888@users.noreply.github.com>
2024-06-29 22:25:48 +0530

    Ready to test

--- Assignment6/Assign6_DataPreprocessing2.py
+++ Assignment6/Assign6_DataPreprocessing2.py
@@ -134,6 +134,75 @@ df.info()
 
 df.head()
 
+#Working with outliers:
+
+def outlier_thresholds(dataframe, col_name, q1=0.25, q3=0.75):
+    quartile1 = dataframe[col_name].quantile(q1)
+    quartile3 = dataframe[col_name].quantile(q3)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+def check_outlier(dataframe, col_name):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    if dataframe[(dataframe[col_name] > up_limit) | (dataframe[col_name] < low_limit)].any(axis=None):
+        return True
+    else:
+        return False
+
+def grab_col_names(dataframe, cat_th=10, car_th=20):
+
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
+    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and dataframe[col].dtypes != "O"]
+    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and dataframe[col].dtypes == "O"]
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O" and col not in num_but_cat]
+
+    print(f"Observations: {dataframe.shape[0]}")
+    print(f"Variables: {dataframe.shape[1]}")
+    print(f"cat_cols: {len(cat_cols)}")
+    print(f"num_cols: {len(num_cols)}")
+    print(f"cat_but_car: {len(cat_but_car)}")
+    print(f"num_but_cat: {len(num_but_cat)}")
+
+    return cat_cols, num_cols, cat_but_car
+
+def grab_outliers(dataframe, col_name, outlier_index=False, f = 5):
+    low, up = outlier_thresholds(dataframe, col_name)
+
+    if dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].shape[0] > 10:
+        print(dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].head(f))
+    else:
+        print(dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))])
+
+    if outlier_index:
+        out_index = dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].index
+        return out_index
+
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+#Let's see which column has outliers...
+for col in num_cols:
+    print(col, check_outlier(df, col))
+ 
+
+#Now replace these outliers with thresholds.
+for col in num_cols:
+    replace_with_thresholds(df, col)
+
+#After replacing, we shouldn't have any outlier. So let's check again.
+for col in num_cols:
+    print(col, check_outlier(df, col))
+
 
 ##Feature Scaling 
 df_test=df[df['Purchase'].isnull()]
